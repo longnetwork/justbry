42b7dfd46d9cb510272c7fb43c3a96ebff002028
SteepPepper <steephairy@gmail.com>
2026-09-12 10:46:10 +0300

    removing base64 encoding layer

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='justbry',
-    version='2.6',
+    version='2.5',
     description='Fully Pythonized Framework for Creating React Applications and Services',
     
     author='Steep Pepper',
